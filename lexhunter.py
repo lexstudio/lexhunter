498d7e3c83c88997ebcc7466eedab20c2f049464
lexstudio <106554826+lexstudio@users.noreply.github.com>
2024-01-11 16:41:27 +0000

    Add files via upload

---
+++ lexhunter.py
@@ -0,0 +1,410 @@
+import time
+import os
+import random
+
+
+while True:
+    command = input("lexhunter1---> ")
+
+    if command == "exit":
+        break
+    
+
+    elif command == "server -host msv//local//mmm":
+        print("connect")
+        time.sleep(1)
+        while True:
+            target_command = input("ms>>>")
+
+            if target_command == "passwd -now":
+                print("pisica234")
+
+            elif target_command == "Webwhere -olx ip 192.168.0.655":
+                print("search with msv")
+                time.sleep(2)
+                print("find starting msv server")
+                time.sleep(6)
+            
+            elif target_command == "ip -a -now":
+                print("The user is online msv stary action")
+                time.sleep(3)
+                print("msv start rempote controll")
+                time.sleep(2)
+                print("access deniet")
+                time.sleep(2)
+                print("redirection on lexhunter api")
+                time.sleep(3)
+                print("hunter injection")
+                time.sleep(30)
+                print("injection failed but find a vulnerabilite")
+                print("msv port open 7121")
+                print("ip port open 7777")
+
+            elif target_command == "/msv.host | -r 7121 --remotelymsv-- start conection -now":
+                print("msv remote start")
+                time.sleep(3)
+                print("host start")
+                time.sleep(19)
+                print("msv conection stabil")
+                print("target name trg")
+
+            elif target_command == "trg\ -d system -now | sim card encypt -p haidacapoti123":
+                print("delete has started")
+                time.sleep(6)
+                print("lost connection too trg")
+
+
+            elif target_command == "all":
+                print("nono")
+
+            if target_command == "exit":
+                print("Ieșire din python target.")
+                break
+
+    elif command == "pnet":
+        os.system("python pnet.py")
+
+    elif command == "start -S server.hunter":
+        print("server start hosting")
+        time.sleep(5)
+        print("server is create")
+        time.sleep(0.3)
+        print("conecting to server")
+        time.sleep(2)
+        while True:
+            servercm = input("server(hun)           %")
+
+            if servercm == "passwd -now":
+                print("pisica234")
+
+            elif servercm == "kill -m 6666":
+                print("kiling m")
+                time.sleep(3)
+                print("1923 host has been kiled")
+            
+            elif servercm == "inject --msf 192.168.0.1":
+                time.sleep(1)
+                print("injecting msf in full network")
+                time.sleep(2)
+                print("network access granded")
+                time.sleep(0.6)
+                print("msf is injected")
+            
+            elif servercm == "start -n msf.exe NETWORK":
+                print("access grandet")
+                time.sleep(5)
+                print("msf.exe is started")
+            elif servercm == "int msf.exe -- if open set passwd 123 connect 123":
+                print("verify the msf has a passwd or a owner")
+                time.sleep(2)
+                print("no owner or passwd has been recorded")
+                time.sleep(0.4)
+                print("passwd is recording")
+                time.sleep(0.2)
+                print("you are the owner of msf.exe")
+            
+            elif servercm == "msf.exe -c (access -all network LOCAL)":
+                print("puting command in msf.exe")
+                time.sleep(0.2)
+                print("access grandet")
+                time.sleep(0.1)
+                print("creating instaler")
+                time.sleep(0.3)
+                print("complete")
+            
+            elif servercm == "import-pkg msf.exe installer.int LOCAL":
+                print("importing")
+                time.sleep(3)
+                print("import complet")
+
+            elif servercm == "int instaler.int":
+                print("downloading")
+                time.sleep(0.6)
+                print("clasify downloading complete 100%")
+                time.sleep(2)
+                print("int downloading complete 100%")
+                time.sleep(0.6)
+                print("contrityt downloading complete 100%")
+                time.sleep(0.3)
+                print("gintry downloading complete 100%w")
+                time.sleep(0.2)
+                print("intialisi downloading complete 100%")
+                time.sleep(0.3)
+                print("scanerss downloading complete 100%")
+                time.sleep(0.7)
+                print("complete")
+            
+            elif servercm == "find ssv":
+                print("emulation:87281927*&102983&@18&#0173#a234747&$^@23*$92487469*&$^$8624^$828^$^8268852")
+                print("ssv:237654826*&^@$%765&&^%$@5875&6577^%&*^%@&^@%&^%$78^@%$7547265476427567627")
+                print("in:2876872634^$*$&^@237487266*$&8^$")
+
+            elif servercm == "fsd -S 2876872634^$*$&^@237487266*$&8^$":
+                print("complete")
+            elif servercm == "atk -wifi 237654826*&^@$%765&&^%$@5875&6577^%&*^%@&^@%&^%$78^@%$7547265476427567627":
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("atk wifi signal send")
+                time.sleep(0.3)
+                print("signal interference wifi ems")
+
+            elif servercm == "emulate --wifi --ems wifi interference LOCALE-- 87281927*&102983&@18&#0173#a234747&$^@23*$92487469*&$^$8624^$828^$^8268852":
+                print("signal corect")
+                time.sleep(2)
+                print("emulation start")
+                time.sleep(0.2)
+                print("emulation will start when you will write start -m")
+
+
+            elif servercm == "start -m":
+                print("emulation will start")
+                time.sleep(7)
+                print("emulation is redy")
+
+            elif servercm == "emulate -openport:8888":
+                print("emulating....")
+                time.sleep(10)
+                print("emulate completed")
+
+            elif servercm == "set -1 wifi:port:8888 -2 LOCAL::openport:8888":
+                print("permision denied")
+
+            elif servercm == "emulate router-update -data set -1 wifi:port:8888 -2 LOCAL wifi::openport:8888":
+                print("emulating...")
+                time.sleep(6)
+                print("sending....")
+                time.sleep(7)
+                print("router accepted the update")
+                
+            elif servercm == "eviluser -u alex -p sun12":
+                print("OK")
+
+            elif servercm == "evilsql -inject alex --wifiport 8888 -admin":
+                print("scaning port 8888")
+                time.sleep(9)
+                print("port 8888 is open")
+                time.sleep(0.5)
+                print("injecting....")
+                time.sleep(1)
+                print("user alex is in wifi as admin")
+                while True:
+                    wificommand = input("wifiadmin-192.168.0.12/24# ")
+
+                    if wificommand == "show passwd":
+                        print("wifipasswd:patra321")
+
+                    elif wificommand == "reset port:8888 -force":
+                        print("reseting....")
+                        time.sleep(2)
+                        print("port:8888")
+
+                    elif wificommand == "safeports start":
+                        print("starting all safeports")
+                        time.sleep(5)
+                        print("50 safeports are active")
+                        print("no errors")
+
+                    elif wificommand == "open -all ports":
+                        print("wait...")
+                        time.sleep(20)
+                        print("1321 ports are open")
+
+                    elif wificommand == "stop -all --public-control-ports":
+                        print("wait")
+                        time.sleep(3)
+                        print("one port has been stoped")
+
+                    elif wificommand == "exit":
+                        break
+
+                    else:
+                        print("error")
+
+                    
+            
+            if servercm == "exit":
+                print("exit server")
+                break
+
+    elif command == "start -i | -api -n openAI -p one12 -id 5555":
+        print("inregister")
+        time.sleep(2)
+        print("api generate lexhunter//::msip727272.1.5")
+
+    elif command == "spp -api -LOCALmsip lexhunter//::msip727272.1.5":
+        print("test connection")
+
+        while True:
+            Pcommand = input("apiconnection::")
+
+            if Pcommand == "exit":
+                print("exit")
+                break
+            elif Pcommand == "request -cn":
+                print("api.openai.com target")
+                time.sleep(0.34)
+                print("time 0.34")
+                time.sleep(3)
+                print("pine restart msv at https://platform.openai.com/account/api-keys")
+
+            elif Pcommand == "inject -SQL 123":
+                print("injecting")
+                time.sleep(4)
+
+            elif Pcommand == "me":
+                print("target|||PHOST 764:234:78634:76354:283746")        
+    
+    elif command == "hello":
+        print("Hello, world!")
+
+    elif command == "wifiscan -LOCAL":
+        print("start scan")
+        time.sleep(2)
+        print("target 192.169.0.5")
+        print("alex 192.168.77.1")
+        print("alex(vmware)  127.0.0.1")
+        time.sleep(7)
+        print("prepare too scan fft")
+        time.sleep(50)
+        print("target 75:273:63:84::48")
+        print("alex 545:346:54:92::555")
+        print("alex(wmware) 788:534:641:77::5532")
+	
+    elif command == "set PHOST 75 -name target":
+        print("start too set PHOST")
+        time.sleep(9)
+        print("PHOST 764:234:78634:76354:283746;;;;22")
+
+    elif command == "start -q2 PHOST 764:234:78634:76354:283746 ":
+        print("search for PHOST")
+        time.sleep(9)
+        print("starting PHOST")
+	
+    elif command == "connect too PHOST  764:234:78634:76354:283746":
+        print("conecting too PHOST")
+        time.sleep(16)
+        while True:
+            Pcommand = input("PHOST\764:234:78634:76354:283746-&&")
+
+            if Pcommand == "exit":
+                print("exit")
+                break
+            
+            elif Pcommand == "inject -SQL 123":
+                print("injecting")
+                time.sleep(4)
+
+            elif Pcommand == "me":
+                print("target|||PHOST 764:234:78634:76354:283746")
+
+    elif command == "ssh --SQL-- 123 -r ":
+        print("Conectare la target...")
+        while True:
+            target_command = input("ms>>>")
+
+            if target_command == "passwd -now":
+                print("pisica234")
+
+            elif target_command == "all":
+                print("nono")
+
+            if target_command == "exit":
+                print("Ieșire din python target.")
+                break
+    elif command == "control-wifi-hunter -2 -name 'alex 5' -passwd patra321":
+        print("search for wifi")
+        time.sleep(3)
+        print("connecting to wifi")
+        time.sleep(5)
+        print("injecting controll -s 999")
+        time.sleep(3)
+        print("injecting wifi-storage -all")
+        time.sleep(6)
+        print("acces storage")
+        time.sleep(3)
+        print("transfer -lexhunter/wificontroll")
+        time.sleep(4)
+        print("set all data")
+        time.sleep(6)
+        print("set a passwd")
+        input("set passwd: ")
+        print("seting passwd")
+        time.sleep(2)
+        print("passwd is set")
+        print("connecting to lexhunter")
+        time.sleep(7)
+        while True:
+            wifi_command = input("ms>>>")
+
+            if wifi_command == "passwd -now":
+                print("lex12")
+
+            elif wifi_command == "target -sn DES":
+                print("target found DESKTOP-")
+                print("no more target was found")
+            elif wifi_command == "wp --connect DESKTOP-":
+                print("wp set in one module")
+                time.sleep(0.2)
+                print("target codet name is 321.222")
+            elif wifi_command == "silent-attack 321.222 -int scan passwd from 321.222":
+                time.sleep(0.6)
+                print("start scan passwd")
+                time.sleep(0.2)
+                print("ask for passwd")
+                time.sleep(0.4)
+                print("start to note the information about the passwd")
+                time.sleep(0.8)
+                print("passwd has 5 letters")
+                print("passwd has 2 number")
+                print("password does not contain repeating letters")
+                time.sleep(2)
+                print("start too make all passwd that can be")
+                time.sleep(14)
+                print("start too ask what passwd is more like the him passwd")
+                time.sleep(30)
+                print("the most good passwd are:yui12 fui63 fuy34 fii84 gir12 fui12 loo23 43ass")
+
+            elif wifi_command == "sil -send --goodpasswd":
+                print("silent this pc")
+                time.sleep(6)
+                print("send goodpasswd")
+                time.sleep(4)
+                print("wait for target too make a noise")
+                time.sleep(5)
+                print("target make a noise")
+                time.sleep(0.2)
+                print("the corect passwd is fui12")
+
+            elif wifi_command == "connect -now 321.222 passwd fui12":
+                os.system("evil-winrm -i 192.168.0.171 -u User -p fui12")
+            if wifi_command == "exit":
+                print("Ieșire din python target.")
+                break
+    else:
+        print("this command is incorect or doesn't exist")
